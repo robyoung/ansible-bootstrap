0eedbb137076dd68dda63ed6ace1dede62cd720f
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-06-30 19:59:20 +0100

    Add comments to bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Root path of ansible-bootstrap
 root_path() {
   pushd $(dirname $0)/.. > /dev/null
-  echo $(pwd -P	)
+  echo $(pwd -P)
   popd > /dev/null
 }
 
+# Link battleschool config into place
 mkdir ~/.battleschool/
 ln -s $(root_path)/config/config.yml ~/.battleschool/
